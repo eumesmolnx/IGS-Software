325ed993fbb104a027fe6a72187e5d704418276f
eumesmolnx <eumesmolnx@yahoo.com>
2022-05-12 22:39:32 -0300

    Create API URLs

--- IGS/urls.py
+++ IGS/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from IGSSoftwareManager.views import EmployeeViewSet,DepartamentViewSet
+
+router = routers.DefaultRouter()
+router.register(r'employee', EmployeeViewSet)
+router.register(r'departament', DepartamentViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
