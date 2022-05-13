f32ca84dd577a6db9d74e5d6f334f3f883e8148c
eumesmolnx <eumesmolnx@yahoo.com>
2022-05-12 22:36:18 -0300

    Create Django REST Framework Viewsets

--- IGSSoftwareManager/views.py
+++ IGSSoftwareManager/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import EmployeeSerializer, DepartamentSerializer
+from .models import Employee,Departament
 
 # Create your views here.
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+# Create your views here.
+class DepartamentViewSet(viewsets.ModelViewSet):
+    queryset = Departament.objects.all()
+    serializer_class = DepartamentSerializer
