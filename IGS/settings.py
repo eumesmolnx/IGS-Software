ec28439c2f686a8cb4581544ede6b0b6ffa230f2
eumesmolnx <eumesmolnx@yahoo.com>
2022-05-12 21:58:02 -0300

    Update Django settings to connect PostgreSQL database

--- IGS/settings.py
+++ IGS/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'IGS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'igsdb',
+        'USER': 'igsuser',
+        'PASSWORD': 'igspass',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
