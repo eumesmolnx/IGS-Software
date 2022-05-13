df782d27d5eb758f8ee24a3c8e59d89ae7001818
eumesmolnx <eumesmolnx@yahoo.com>
2022-05-12 22:12:37 -0300

    Create Employee and Departament models

--- IGSSoftwareManager/models.py
+++ IGSSoftwareManager/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=255)
+    departament = models.ForeignKey('Departament',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Departament(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
