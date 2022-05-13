c4c1513fb4a7d9148f325292a1f67e98a99ba5a2
eumesmolnx <eumesmolnx@yahoo.com>
2022-05-12 22:28:50 -0300

    Create Super User admin + Load Employees and Departaments into database

--- IGSSoftwareManager/admin.py
+++ IGSSoftwareManager/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Employee,Departament
 # Register your models here.
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("name","email","departament")
+
+@admin.register(Departament)
+class DepartamentAdmin(admin.ModelAdmin):
+    list_display = ("name",)
